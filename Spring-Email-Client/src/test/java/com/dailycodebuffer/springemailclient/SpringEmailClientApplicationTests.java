dab99f11fc8e6a4fc0a012eca02fd1e52f56d3c7
NAG <arnab.nag@capgemini.com>
2023-03-02 10:38:11 +0530

    microservice commit 3

--- Spring-Email-Client/src/test/java/com/dailycodebuffer/springemailclient/SpringEmailClientApplicationTests.java
+++ Spring-Email-Client/src/test/java/com/dailycodebuffer/springemailclient/SpringEmailClientApplicationTests.java
@@ -3,7 +3,7 @@ package com.dailycodebuffer.springemailclient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.capg.profileJWT.ProfileJwtApplicationTests;
+
 
 @SpringBootTest(classes = {SpringEmailClientApplicationTests.class})
 class SpringEmailClientApplicationTests {
